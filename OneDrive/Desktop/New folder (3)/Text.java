23e903cc0d5923a03ddc4d624bc1278a2fa0bfa8
Hariharamuthumarish <harip185001@gmail.com>
2022-05-19 18:17:13 +0530

    Done

---
+++ OneDrive/Desktop/New folder (3)/Text.java
@@ -0,0 +1,11 @@
+public class Text {
+    public static void main(String[] args) {
+        int a=10;
+        {
+            int x=a;
+            System.out.println(x);
+        }
+        System.out.println(a);
+    }
+    
+}
